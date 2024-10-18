64fbbc20870706a018a74e295c437aaa5313a104
Nur Alam <nuralam.rsc@gmail.com>
2024-10-19 05:32:15 +0600

    feat: marquee animation add to logo ticker section

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import acmeLogo from "../assets/images/acme.png";
 import apexLogo from "../assets/images/apex.png";
@@ -5,6 +7,8 @@ import celestiaLogo from "../assets/images/celestia.png";
 import echoLogo from "../assets/images/echo.png";
 import pulseLogo from "../assets/images/pulse.png";
 import quantumLogo from "../assets/images/quantum.png";
+
+import { motion } from "framer-motion";
 
 const images = [
   { id: 1, src: acmeLogo, alt: "Acme logo" },
@@ -22,8 +26,17 @@ export const LogoTicker = () => {
         <h2 className="text-xl text-center text-white/70">
           Trusted by the world&apos;s most innovative teams
         </h2>
-        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))]">
-          <div className="flex gap-16">
+        <div className="flex overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-10 after:w-10 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))] before:z-10">
+          <motion.div
+            initial={{ translateX: 0 }}
+            animate={{ translateX: "-50%" }}
+            transition={{
+              duration: 5,
+              ease: "linear",
+              repeat: Infinity,
+            }}
+            className="flex gap-16 flex-none pr-16"
+          >
             {images.map((image) => (
               <Image
                 src={image.src}
@@ -32,7 +45,15 @@ export const LogoTicker = () => {
                 className="flex-none h-8 w-auto"
               />
             ))}
-          </div>
+            {images.map((image) => (
+              <Image
+                src={image.src}
+                key={image.id}
+                alt={image.alt}
+                className="flex-none h-8 w-auto"
+              />
+            ))}
+          </motion.div>
         </div>
       </div>
     </div>

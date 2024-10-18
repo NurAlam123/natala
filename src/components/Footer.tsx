96f584e91123800fa49cc2f5912f214a7b0864d8
Nur Alam <nuralam.rsc@gmail.com>
2024-10-19 04:35:29 +0600

    feat: add footer and call to action section

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import InstaIcon from "../assets/icons/instagram.svg";
+import XSocial from "../assets/icons/x.svg";
+import YoutubeIcon from "../assets/icons/youtube.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-black text-white/60 border-t border-white/20 py-5">
+      <div className="container">
+        <div className="flex flex-col sm:flex-row sm:justify-between gap-4">
+          <div className="text-center">
+            &copy; 2024 Natala. All rights reserved.
+          </div>
+          <ul className="flex justify-center gap-2.5">
+            <li>
+              <InstaIcon />
+            </li>
+            <li>
+              <XSocial />
+            </li>
+            <li>
+              <YoutubeIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};

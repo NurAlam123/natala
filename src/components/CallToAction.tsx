feaff2f6c31bd8159e5a04adc6376ae2ea65d302
Nur Alam <nuralam.rsc@gmail.com>
2024-10-19 16:38:47 +0600

    feat: add motion to call to action section

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,21 +1,51 @@
+"use client";
+
 import helixImage from "../assets/images/helix2.png";
 import emojiStarImage from "../assets/images/emojistar.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const CallToAction = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end end"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [40, -40]);
+
   return (
-    <div className="bg-black text-white py-[72px] sm:py-24 text-center overflow-clip">
+    <div
+      className="bg-black text-white py-[72px] sm:py-24 text-center overflow-clip"
+      ref={containerRef}
+    >
       <div className="container max-w-xl relative">
-        <Image
-          src={helixImage}
-          alt=""
-          className="absolute top-6 left-[calc(100%+36px)]"
-        />
-        <Image
-          src={emojiStarImage}
-          alt=""
-          className="absolute -top-[60px] right-[calc(100%-24px)]"
-        />
+        <motion.div
+          style={{
+            translateY,
+          }}
+        >
+          <Image
+            src={helixImage}
+            alt=""
+            className="absolute top-6 left-full"
+            draggable={false}
+          />
+        </motion.div>
+        <motion.div
+          style={{
+            translateY,
+          }}
+        >
+          <Image
+            src={emojiStarImage}
+            alt=""
+            className="absolute -top-[60px] right-full"
+            draggable={false}
+          />
+        </motion.div>
         <h2 className="text-5xl font-bold tracking-tighter sm:text-6xl">
           Get instant access
         </h2>
